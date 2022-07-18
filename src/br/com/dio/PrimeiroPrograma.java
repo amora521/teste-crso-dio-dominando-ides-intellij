bb7a65d03236ae81f5953f596dcd0b17e8b7426d
Areum <paloma.mmendonca@hotmail.com>
2022-07-18 11:50:53 -0300

    criando class Livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -7,6 +7,8 @@ public class PrimeiroPrograma {
         Gato gato = new Gato();
         System.out.println(gato);
 
+        Livro livro1 = new Livro("Os instrumentos mortais - Cidade dos Ossos", 458);
+        System.out.println(livro1);
 
         /*int a = 5;
         int b = 3;
@@ -14,3 +16,38 @@ public class PrimeiroPrograma {
     }
 
 }
+
+class Livro {
+    private String nome;
+    private Integer nunPag;
+
+    public Livro(String nome, Integer nunPag) {
+        this.nome = nome;
+        this.nunPag = nunPag;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNunPag() {
+        return nunPag;
+    }
+
+    public void setNunPag(Integer nunPag) {
+        this.nunPag = nunPag;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", nunPag=" + nunPag +
+                '}';
+    }
+}
+
